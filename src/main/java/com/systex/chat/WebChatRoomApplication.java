c57998300ed9a64f904dcf364f8f9ebaca48f7cc
2000159 <tlksimba519@gmail.com>
2020-05-28 16:39:56 +0800

    init

---
+++ src/main/java/com/systex/chat/WebChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.systex.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systex/chat/WebChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systex.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
