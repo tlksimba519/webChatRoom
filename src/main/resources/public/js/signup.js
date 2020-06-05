5a9625b2791019e2307ef3d6beb94030d2bcbf70
2000159 <tlksimba519@gmail.com>
2020-06-05 10:58:02 +0800

    登入系統子系統 註冊 補上Controller,前端html&css,DB所需額外function

--- src/main/resources/public/js/signup.js
+++ src/main/resources/public/js/signup.js
@@ -1,3 +1,5 @@
+var temp = [];
+
 $(document).ready(function(){
 	
 	$.ajax({
@@ -8,7 +10,6 @@ $(document).ready(function(){
 		  complete : function(response){
 			  
 			  var data = response.responseJSON;
-			  var temp = [];
 			  
 			  for(var i=1;i<=Object.keys(data).length;i++){
 				  
@@ -24,3 +25,28 @@ $(document).ready(function(){
 	});
 	
 });
+$("#signup").validate({
+	  rules: {
+	    UserName: {
+	    	required :true,
+	    	alreadyUsed :true
+	    },
+	    Password: "required",
+	    PasswordTwice:{
+	    	equalTo :"#Password"
+	    }
+	  }
+});
+jQuery.validator.addMethod("alreadyUsed", function(value, element) 
+		{
+		    value  = $.trim(value);
+		    if(temp.indexOf(value)==-1)
+		    {
+		       return true;
+		    }
+		    else
+		    {
+		        return false;
+		    }
+
+		},"Username is already used. Sorry.");
